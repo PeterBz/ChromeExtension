d09ea236aa6613b7766dc5f9625de75f6404875d
Peter <peter_brunzel@web.de>
2026-03-03 17:36:16 +0100

    working on local storage

--- index.js
+++ index.js
@@ -3,10 +3,21 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+// localStorage.setItem("myName", "Peter")
+// let name = localStorage.getItem("myName")
+// console.log(name)
+localStorage.clear()
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    
+    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
     renderLeads()
+
+    console.log(localStorage.getItem("myLeads"))
 })
 
 function renderLeads() {
