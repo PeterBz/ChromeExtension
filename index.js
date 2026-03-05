9628c005d643ae7d6be8338d3c3784fee2b12839
Peter <peter_brunzel@web.de>
2026-03-05 07:30:06 +0100

    saving a hard coded tab, next up is the tab using chrome api

--- index.js
+++ index.js
@@ -1,43 +1,49 @@
-let myLeads = []
+let myLeads = [] 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-const deleteBtn = document.getElementById("delete-btn")
 const deleteAllBtn = document.getElementById("deleteAll-btn")
-
+const tabBtn = document.getElementById("tab-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-if(leadsFromLocalStorage) { 
+if (leadsFromLocalStorage) { 
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 } 
 
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
+tabBtn.addEventListener("click", function() { 
+    myLeads.push(tabs[0].url) 
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
 deleteAllBtn.addEventListener("dblclick", function() { 
-    
     localStorage.clear()
-    myLeads =[]
-    renderLeads()
-    console.log("double clicked success")
+    myLeads = []
+    render(myLeads)
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    
-    localStorage.setItem("myLeads" , JSON.stringify(myLeads))
-    renderLeads()
-
-    console.log(localStorage.getItem("myLeads"))
+    if (inputEl.value) {
+        myLeads.push(inputEl.value)
+        inputEl.value = ""
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    }
 })
 
-function renderLeads() {
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]} 
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]} 
                 </a> 
                 <button class="delete-single" data-index="${i}">🗑️</button>
             </li>   
@@ -45,23 +51,17 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems  
 
-    // Event Listener zum delete Icon hinzufügen 
-    const deleteSingleLink = document.querySelectorAll(".delete-single")
-    deleteSingleLink.forEach( btn => { 
-        btn.addEventListener("click", function (e) { 
-            const index = e.target.dataset.index
+    const deleteSingleButtons = document.querySelectorAll(".delete-single")
+    deleteSingleButtons.forEach(btn => { 
+        btn.addEventListener("click", function(e) { 
+            const index = e.currentTarget.dataset.index
             deleteLead(index)
         })
     })
 }
 
 function deleteLead(index) {
-    // 1. Aus dem Array entfernen
     myLeads.splice(index, 1)
-    // 2. Im LocalStorage aktualisieren
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    // 3. Neu anzeigen
-    renderLeads()
+    render(myLeads) 
 }
-
-
